369dfe2ed402d6801b150e5f78313a234361cd8d
HP-PC <HP-PC@HP-PCX>
2015-10-05 10:52:35 +0530

    commit new

--- JavaApplication11/src/TEST1.java
+++ JavaApplication11/src/TEST1.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +11,9 @@ import java.util.Scanner;
  * @author Vishva
  */
 public class TEST1 {
+    public static void main(String[] args) {
+        System.out.println("TEST");
+    }
     
 
 }
